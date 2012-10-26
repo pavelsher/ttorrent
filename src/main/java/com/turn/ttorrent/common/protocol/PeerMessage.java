ec4de2fb643cad209b25e836e4cfb255eeecd009
Pavel Sher <pavel.sher@gmail.com>
2012-10-26 21:53:21 +0200

    fix birfield message length calculation

--- src/main/java/com/turn/ttorrent/common/protocol/PeerMessage.java
+++ src/main/java/com/turn/ttorrent/common/protocol/PeerMessage.java
@@ -409,7 +409,9 @@ public abstract class PeerMessage {
 		}
 
 		public static BitfieldMessage craft(BitSet availablePieces) {
-			byte[] bitfield = new byte[availablePieces.length()/8 + 1];
+      int len = availablePieces.length()/8;
+      if (availablePieces.length() % 8 > 0) len++;
+			byte[] bitfield = new byte[len];
 			for (int i=availablePieces.nextSetBit(0); i >= 0;
 					i=availablePieces.nextSetBit(i+1)) {
 				bitfield[i/8] |= 1 << (7 -(i % 8));
